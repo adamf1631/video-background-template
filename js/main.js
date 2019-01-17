42a509097198f1ebb07a40375e58bf1d28abf15c
Adam Falchetta <adamf1631@yahoo.com>
2019-01-16 20:03:12 -0500

    Video background template html5 css 3
    Use a high quality mp4 for videos on each of the 4 pages. Links can be
    found in top part of html page body section. (Images not included.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+'use strict()';
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      toolbarEnabled: true
+    });
+  });
+
+
+  
